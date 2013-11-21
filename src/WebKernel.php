700929fb9c577d71a7bb560017637abd053f7416
Paul M. Jones <pmjones88@gmail.com>
2013-11-21 15:02:11 -0600

    go from route/dispatch/respond to request/response

--- src/WebKernel.php
+++ src/WebKernel.php
@@ -6,24 +6,20 @@ class WebKernel extends ProjectKernel
     public function __invoke()
     {
         parent::__invoke();
-        $this->setProperties();
-        $this->route();
-        $this->dispatch();
-        $this->respond();
-    }
-    
-    protected function setProperties()
-    {
+        
         $this->logger     = $this->di->get('logger');
         $this->request    = $this->di->get('web_request');
         $this->response   = $this->di->get('web_response');
         $this->router     = $this->di->get('web_router');
         $this->dispatcher = $this->di->get('web_dispatcher');
+        
+        $this->request();
+        $this->response();
     }
     
-    protected function route()
+    protected function request()
     {
-        $this->logger->debug(__METHOD__);
+        $this->logger->debug(__METHOD__ . ' routing');
         
         $route = $this->router->match(
             $this->request->url->get(PHP_URL_PATH),
@@ -33,58 +29,54 @@ class WebKernel extends ProjectKernel
         if ($route) {
             $this->request->params->set($route->params);
         } else {
-            $this->logger->debug(__METHOD__ . ' no route');
+            $this->logger->debug(__METHOD__ . ' routing: no route');
             $this->request->params->set(array(
                 'controller' => 'no_route',
-                'request' => $this->request,
-                'response' => $this->response,
             ));
         }
 
         $routes = $this->router->getLog();
         if (! $routes) {
-            $this->logger->debug('No routes in router.');
-            return;
-        }
-            
-        foreach ($routes as $route) {
-            foreach ($route->debug as $message) {
-                $message = __METHOD__
-                         . ' ' . ($route->name ? $route->name : $route->path)
-                         . ' ' . $message;
-                $this->logger->debug($message);
+            $this->logger->debug(__METHOD__ . ' routing: no routes in router');
+        } else {
+            foreach ($routes as $route) {
+                foreach ($route->debug as $message) {
+                    $name = $route->name
+                          ? $route->name
+                          : $this->request->method->get() . ' ' . $route->path;
+                    $message = __METHOD__ . " routing: $name $message";
+                    $this->logger->debug($message);
+                }
             }
         }
-    }
-    
-    protected function dispatch()
-    {
-        $this->logger->debug(__METHOD__);
         
         try {
+            $this->logger->debug(__METHOD__ . ' dispatch');
             $this->dispatcher->__invoke($this->request->params->get());
         } catch (\Aura\Dispatcher\Exception\ObjectNotDefined $e) {
-            $this->logger->debug(__METHOD__ . ' no controller');
+            $this->logger->debug(__METHOD__ . ' dispatch: no controller');
             $dispatcher->__invoke(array(
                 'controller' => 'no_controller',
-                'request' => $this->request,
-                'response' => $this->response,
             ));
         }
     }
     
-    protected function respond()
+    protected function response()
     {
         $this->logger->debug(__METHOD__);
         
         // send the response status line
-        header($this->response->status->get(), true, $this->response->status->getCode());
-    
+        header(
+            $this->response->status->get(),
+            true,
+            $this->response->status->getCode()
+        );
+        
         // send non-cookie headers
         foreach ($this->response->headers->get() as $label => $value) {
             header($label, $value);
         }
-    
+        
         // send cookies
         foreach ($this->response->cookies->get() as $name => $cookie) {
             setcookie(
@@ -97,7 +89,7 @@ class WebKernel extends ProjectKernel
                 $cookie['httponly']
             );
         }
-    
+        
         // send content, and done!
         echo $this->response->content->get();
     }
