61635a0d9ab450f44038c8c53c91d5719917d5c6
Paul M. Jones <pmjones88@gmail.com>
2013-11-26 23:00:01 -0600

    move setup to kernel factory

---
+++ src/WebKernelFactory.php
@@ -0,0 +1,9 @@
+<?php
+namespace Aura\Web_Kernel;
+
+use Aura\Project_Kernel\ProjectKernelFactory;
+
+class WebKernelFactory extends ProjectKernelFactory
+{
+    protected $class = 'Aura\Web_Kernel\WebKernel';
+}
