4af4a8bf0c069dd6350eac33a0c58127de08b4ad
Paul M. Jones <pmjones88@gmail.com>
2013-12-17 16:21:58 -0600

    use the new project kernel script

--- scripts/kernel.php
+++ scripts/kernel.php
@@ -11,28 +11,13 @@
  */
 namespace Aura\Web_Kernel;
 
-use Aura\Project_Kernel\ProjectKernelFactory;
-
-// the project base directory, relative to
-// {$project}/vendor/aura/web_kernel/scripts/kernel.php
+// get the project kernel
 $base = dirname(dirname(dirname(dirname(__DIR__))));
+$project_kernel = require "{$base}/vendor/aura/project-kernel/scripts/kernel.php";
 
-// the project config mode
-$file = str_replace("/", DIRECTORY_SEPARATOR, "{$base}/config/_mode");
-$mode = trim(file_get_contents($file));
-if (! $mode) {
-    $mode = "default";
-}
-
-// composer autoloader, add project src/ directory
-$loader = require "{$base}/vendor/autoload.php";
-$loader->add('', "{$base}/src");
-
-// project config
-$project_kernel_factory = new ProjectKernelFactory;
-$project_kernel = $project_kernel_factory->newInstance($base, $mode, $loader);
+// invoke it to get the DI container
 $di = $project_kernel->__invoke();
 
-// run the web kernel
-$web_kernel = $di->get('web_kernel');
+// create a web kernel instance from the DI container and invoke it
+$web_kernel = $di->newInstance('Aura\Web_Kernel\WebKernel');
 $web_kernel->__invoke();
