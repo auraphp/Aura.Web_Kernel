3bce1bd2291e0ec39ee5216d66a403f946eb56d1
Paul M. Jones <pmjones88@gmail.com>
2013-12-17 16:21:32 -0600

    don't retain a web_kernel service

--- config/default/define.php
+++ config/default/define.php
@@ -6,7 +6,6 @@ $di->set('web_request', $di->lazyNew('Aura\Web\Request'));
 $di->set('web_response', $di->lazyNew('Aura\Web\Response'));
 $di->set('web_router', $di->lazyNew('Aura\Router\Router'));
 $di->set('web_dispatcher', $di->lazyNew('Aura\Dispatcher\Dispatcher'));
-$di->set('web_kernel', $di->lazyNew('Aura\Web_Kernel\WebKernel'));
 
 /**
  * Aura\Web_Kernel\AbstractController
