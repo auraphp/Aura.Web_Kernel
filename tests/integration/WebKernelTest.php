721ba2c4f606891c2757ef7c6db16c1a9052fecd
Paul M. Jones <pmjones88@gmail.com>
2013-12-18 17:04:04 -0600

    add tests

--- tests/integration/WebKernelTest.php
+++ tests/integration/WebKernelTest.php
@@ -5,16 +5,42 @@ class WebKernelTest extends \PHPUnit_Framework_TestCase
 {
     protected function exec()
     {
+        // force into integration mode
         $_ENV['AURA_CONFIG_MODE'] = 'integration';
+        
+        // always have an HTTP_HOST or request uri won't get put together
+        $_SERVER['HTTP_HOST'] = 'example.com';
         require dirname(dirname(__DIR__)) . '/scripts/kernel.php';
         $this->web_kernel = $web_kernel;
     }
     
-    public function test()
+    public function testHelloWorld()
     {
-        $_SERVER['REQUEST_METHOD'] = 'FAKE';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
         $_SERVER['REQUEST_URI'] = '/';
         $this->exec();
-        var_dump($this->web_kernel->responder->content);
+        $expect = 'Hello World!';
+        $actual = $this->web_kernel->responder->content;
+        $this->assertSame($expect, $actual);
+    }
+    
+    public function testHelloWorldViaIndexPhp()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['REQUEST_URI'] = '/index.php';
+        $this->exec();
+        $expect = 'Hello World!';
+        $actual = $this->web_kernel->responder->content;
+        $this->assertSame($expect, $actual);
+    }
+    
+    public function testMissingRoute()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['REQUEST_URI'] = '/no-such-route';
+        $this->exec();
+        $expect = 'No route for GET /no-such-route';
+        $actual = trim($this->web_kernel->responder->content);
+        $this->assertSame($expect, $actual);
     }
 }
