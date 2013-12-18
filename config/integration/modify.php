94140d4c7c03dec63bcc0e480308000f618a6d3d
Paul M. Jones <pmjones88@gmail.com>
2013-12-18 17:53:14 -0600

    set headers and cookies to increase test coverage

--- config/integration/modify.php
+++ config/integration/modify.php
@@ -5,11 +5,12 @@
 $request = $di->get('web_request');
 $response = $di->get('web_response');
 $router = $di->get('web_router');
-$dispatcher = $di->get('web_dispatcher');
 
 $router->add(null, '/aura/web-kernel/integration/hello')
     ->addValues(array(
         'controller' => function () use ($request, $response) {
+            $response->headers->set('X-Hello', 'World');
+            $response->cookies->set('hello', 'world');
             $response->content->set('Hello World!');
         },
     ));
