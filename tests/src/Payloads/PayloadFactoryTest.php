e18f6a32d2d06c17ddd03ec70079db158b553512
Moshe Brevda <mbrevda@gmail.com>
2014-12-12 12:46:31 +0200

    Add payload assert

--- tests/src/Payloads/PayloadFactoryTest.php
+++ tests/src/Payloads/PayloadFactoryTest.php
@@ -6,8 +6,65 @@ use Tarcha\WebKernel\Tests\Test;
 
 class PayloadFactoryTest extends Test
 {
-    public function testPayloadNoContent()
-    {
+    protected $ns = 'Tarcha\WebKernel\Payloads';
 
+    public function assertPayload($payload)
+    {
+        static $container;
+        $ns = 'Tarcha\WebKernel\Payloads\\';
+
+        if (empty($container)) {
+            $container = $this->newContainer();
+        }
+
+        $this->assertInstanceOf(
+            $ns . $payload,
+            $container->newInstance($ns . $payload)
+        );
+    }
+
+    public function testAlreadyExists()
+    {
+        $this->assertPayload('AlreadyExists');
+    }
+
+    public function testCreated()
+    {
+        $this->assertPayload('Created');
+    }
+
+    public function testDbError()
+    {
+        $this->assertPayload('DbError');
+    }
+
+    public function testError()
+    {
+        $this->assertPayload('Error');
+    }
+
+    public function testInvalid()
+    {
+        $this->assertPayload('Invalid');
+    }
+
+    public function testNoContent()
+    {
+        $this->assertPayload('NoContent');
+    }
+
+    public function testNotFound()
+    {
+        $this->assertPayload('NotFound');
+    }
+
+    public function testNotRecognized()
+    {
+        $this->assertPayload('NotRecognized');
+    }
+
+    public function testSuccess()
+    {
+        $this->assertPayload('Success');
     }
 }
